54886fc5b2864e8798abe645f0994f079428e5bf
anisaputrisetyaningrum@mhs.itenas.ac.id <anisaputrisetyaningrum@mhs.itenas.ac.id>
2020-11-11 08:09:36 +0700

    carrer

--- application/controllers/Halaman.php
+++ application/controllers/Halaman.php
@@ -233,4 +233,10 @@ class Halaman extends CI_Controller {
 		$this->load->view('halaman/templates/js');
         $this->load->view('halaman/templates/footer');
 	}
+
+	public function carrer(){
+		$this->load->views('halaman/templates/header');
+		$this->load->views('halaman/carrer');
+		$this->load->viewa('halaman/templates/footer');
+	}
 }
