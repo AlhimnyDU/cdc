3d762477de3f77d2e52964fa2b4b6d22e2f3b488
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2020-11-19 14:48:14 +0700

    test

--- application/views/perusahaan/home.php
+++ application/views/perusahaan/home.php
@@ -1,6 +1,6 @@
 <?php if (empty($mengikuti)) { ?>
     <!-- page content -->
-    <div id="tambahModal" class="modal fade show" role="dialog">
+    <div id="tambahModal" class="modal fade show" role="dialog" aria-modal="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
