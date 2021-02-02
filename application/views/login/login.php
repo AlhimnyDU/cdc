f31dd7e41ec9314063bb788cd87314da34c78ca1
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-02-02 10:54:06 +0700

    Pembaharuan

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -64,29 +64,31 @@
                   <span class="glyphicon glyphicon-credit-card form-control-feedback left" aria-hidden="true"></span>
                 </div>
               </div>
-              <div class="col-md-12 form-group has-feedback">
-                <input type="text" class="form-control has-feedback-left" name="nama" placeholder="Nama Lengkap" required="" />
-                <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-              </div>
-              <div class="col-md-12 form-group has-feedback">
-                <input type="email" class="form-control has-feedback-left" name="email" placeholder="Email" required="" />
-                <span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
-              </div>
-              <div class="col-md-12 form-group has-feedback">
-                <input type="password" class="form-control has-feedback-left" id="password" name="password" placeholder="Password" required="" />
-                <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
-              </div>
-              <div class="col-md-12 form-group has-feedback">
-                <input type="password" class="form-control has-feedback-left" id="conf_password" name="conf_password" placeholder="Re-Password" required="" />
-                <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
-              </div>
-              <div class="col-md-12 form-group has-feedback">
-                <input type="text" class="form-control has-feedback-left" name="telp" placeholder="Telepon" required="" />
-                <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
-              </div>
-              <div class="col-md-12 form-group has-feedback">
-                <br>
-                <textarea type="text" class="form-control" placeholder="Alamat" name="alamat" required=""></textarea>
+              <div>
+                <div class="col-md-12 form-group has-feedback">
+                  <input type="text" class="form-control has-feedback-left" name="nama" placeholder="Nama Lengkap" required="" />
+                  <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+                </div>
+                <div class="col-md-12 form-group has-feedback">
+                  <input type="email" class="form-control has-feedback-left" name="email" placeholder="Email" required="" />
+                  <span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
+                </div>
+                <div class="col-md-12 form-group has-feedback">
+                  <input type="password" class="form-control has-feedback-left" id="password" name="password" placeholder="Password" required="" />
+                  <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
+                </div>
+                <div class="col-md-12 form-group has-feedback">
+                  <input type="password" class="form-control has-feedback-left" id="conf_password" name="conf_password" placeholder="Re-Password" required="" />
+                  <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
+                </div>
+                <div class="col-md-12 form-group has-feedback">
+                  <input type="text" class="form-control has-feedback-left" name="telp" placeholder="Telepon" required="" />
+                  <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
+                </div>
+                <div class="col-md-12 form-group has-feedback">
+                  <br>
+                  <textarea type="text" class="form-control" placeholder="Alamat" name="alamat" required=""></textarea>
+                </div>
               </div>
               <hr>
               <div>
