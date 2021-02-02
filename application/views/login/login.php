765ea462b903a735500f2063021c23f34f9c2ec3
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-02-02 11:01:11 +0700

    Pembaharuan

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -75,17 +75,18 @@
                 </div>
                 <div class="col-md-12 form-group has-feedback">
                   <input type="password" class="form-control has-feedback-left" id="password" name="password" placeholder="Password" required="" />
-                  <span class="glyphicon glyphicon-key form-control-feedback left" aria-hidden="true"></span>
+                  <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
                 </div>
                 <div class="col-md-12 form-group has-feedback">
                   <input type="password" class="form-control has-feedback-left" id="conf_password" name="conf_password" placeholder="Re-Password" required="" />
-                  <span class="glyphicon glyphicon-key form-control-feedback left" aria-hidden="true"></span>
+                  <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
                 </div>
                 <div class="col-md-12 form-group has-feedback">
                   <input type="text" class="form-control has-feedback-left" name="telp" placeholder="Telepon" required="" />
-                  <span class="glyphicon glyphicon-call form-control-feedback left" aria-hidden="true"></span>
+                  <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
                 </div>
                 <div class="col-md-12 form-group has-feedback">
+                  <br>
                   <textarea type="text" class="form-control" placeholder="Alamat" name="alamat" required=""></textarea>
                 </div>
               </div>
