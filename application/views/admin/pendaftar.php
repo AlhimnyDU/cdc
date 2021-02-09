43cf120946b8b71ddeeada05b78e39f78cf2955a
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-02-09 21:33:40 +0700

    Perubahan

--- application/views/admin/pendaftar.php
+++ application/views/admin/pendaftar.php
@@ -36,9 +36,12 @@
                                             <th>No HP</th>
                                             <th>Email</th>
                                             <th>Keahlian</th>
-                                            <!-- <?php // foreach ($berkas as $row) { ?>
-                                                <th><?php // echo $row->nama_syarat ?></th>
-                                            <?php // } ?> -->
+                                            <!-- <?php // foreach ($berkas as $row) { 
+                                                    ?>
+                                                <th><?php // echo $row->nama_syarat 
+                                                    ?></th>
+                                            <?php // } 
+                                            ?> -->
                                             <th>Aksi</th>
                                         </tr>
                                     </thead>
@@ -65,8 +68,10 @@
                                                 <!-- <?php// foreach ($berkas as $r) {
                                                            // foreach ($data as $d) {
                                                               //  if (($r->nama_syarat == $d->nama_berkas) && ($row->id_akun == $d->id_akun)) { ?>
-                                                            <td><a href="<?php // echo base_url('assets/upload/berkas/' . $d->file) ?>">Ada</a></td>
-                                                       // <?php } ?>
+                                                            <td><a href="<?php // echo base_url('assets/upload/berkas/' . $d->file) 
+                                                                            ?>">Ada</a></td>
+                                                        <?php // } 
+                                                        ?>
                                                 <?php// }
                                                       //  } ?> -->
                                                 <td>
