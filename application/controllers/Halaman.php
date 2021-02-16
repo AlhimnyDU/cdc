7a4ce7846026ebe8d7295842e8b13edf8021ccea
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-02-17 00:42:34 +0700

    Perubahan

--- application/controllers/Halaman.php
+++ application/controllers/Halaman.php
@@ -130,12 +130,12 @@ class Halaman extends CI_Controller
 			} else {
 				$this->session->set_flashdata('failed', "Tambah Gagal");
 			}
-			redirect($_SERVER['HTTP_REFERER']);
+			redirect('halaman/formulirRegistrasi']);
 		} else {
 			$this->session->set_flashdata('telah_daftar', TRUE);
-			redirect($_SERVER['HTTP_REFERER']);
+			redirect('halaman/formulirRegistrasi']);
 		}
-		redirect($_SERVER['HTTP_REFERER']);
+		redirect('halaman/formulirRegistrasi'));
 	}
 
 	public function upload_pernyataan()
@@ -148,7 +148,7 @@ class Halaman extends CI_Controller
 		$upload = $this->upload->do_upload('pernyataan');
 		if (empty($upload)) {
 			$this->session->set_flashdata('failed', "Tambah Gagal");
-			redirect($_SERVER['HTTP_REFERER']);
+			redirect('halaman/formulirRegistrasi'));
 		}
 	}
 
