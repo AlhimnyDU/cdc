0fe3f1ff2bdbfc268b3073759b113ae2b79f982b
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-03-24 22:48:59 +0700

    oke2

---
+++ application/views/halaman/templates/jobfair_home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CDC | ITENAS</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link rel="icon" href="https://www.itenas.ac.id/wp-content/uploads/2020/01/cropped-logo_itenas_aja-1-192x192.png" sizes="192x192">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/home/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="<?php echo base_url() ?>assets/home/assets/animatedmodal/css/animate.min.css" rel="stylesheet">
+
+
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url() ?>assets/home/assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css" />
+
+  <!-- =======================================================
+  * Template Name: Flexor - v2.4.0
+  * Template URL: https://bootstrapmade.com/flexor-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <main id="main">
