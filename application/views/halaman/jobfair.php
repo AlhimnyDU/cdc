49260778770448660b3904e216474298c9b8e67f
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-03-28 23:40:42 +0700

    add

--- application/views/halaman/jobfair.php
+++ application/views/halaman/jobfair.php
@@ -53,11 +53,11 @@
 
         .content {
             position: none;
-            height: 50%;
+            height: auto;
             bottom: 0;
             background: rgba(0, 0, 0, 0.5);
             color: #f1f1f1;
-            width: 100%;
+            width: auto;
             padding: 0px;
         }
     }
