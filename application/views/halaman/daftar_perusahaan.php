d64f985460916113fccb4b53aae9f3ae2649c10b
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-04-26 00:31:38 +0700

    daftar_perusahaan

--- application/views/halaman/daftar_perusahaan.php
+++ application/views/halaman/daftar_perusahaan.php
@@ -11,45 +11,43 @@
                     <div class="card-body">
                         <form id="formulir" action="<?php echo site_url('login/addPerusahaan/') ?>" method="post" enctype="multipart/form-data">
                             <div class="modal-body">
-                                <div class="alert alert-light" role="alert">
-                                    <h4>Isikan Data Perusahaan</h4>
-                                    <hr>
-                                    <div class="form-group">
-                                        <label>Nama Perusahaan / Instansi</label>
-                                        <input type="text" class="form-control has-feedback-left" name="nama_perusahaan" placeholder="Nama Perusahaan" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Email Perusahaan</label>
-                                        <input type="email" class="form-control has-feedback-left" name="email" placeholder="Email" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Password</label>
-                                        <input type="password" class="form-control has-feedback-left" id="password" name="password" placeholder="Password" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Confirm Password</label>
-                                        <input type="password" class="form-control has-feedback-left" id="conf_password" name="conf_password" placeholder="Re-Password" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Telepon Perusahaan</label>
-                                        <input type="text" class="form-control has-feedback-left" name="telp" placeholder="Telepon Perusahaan" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Alamat Perusahaan</label>
-                                        <textarea type="text" class="form-control has-feedback-left" placeholder="Alamat Perusahaan" name="alamat" required=""></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Nama PIC / CP / Penanggung Jawab Akun</label>
-                                        <input type="text" class="form-control has-feedback-left" name="nama_pj" placeholder="Nama Penanggung Jawab" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Telepon PIC / CP / Penanggung Jawab Akun</label>
-                                        <input type="text" class="form-control has-feedback-left" name="telp_pj" placeholder="Telepon Penanggung Jawab" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Upload Logo Perusahaan</label>
-                                        <input type="file" class="dropify" height="100" name="logo" data-max-file-size="1M" data-allowed-file-extensions="jpg jpeg png" required="">
-                                    </div>
+                                <h4>Isikan Data Perusahaan</h4>
+                                <hr>
+                                <div class="form-group">
+                                    <label>Nama Perusahaan / Instansi</label>
+                                    <input type="text" class="form-control has-feedback-left" name="nama_perusahaan" placeholder="Nama Perusahaan" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Email Perusahaan</label>
+                                    <input type="email" class="form-control has-feedback-left" name="email" placeholder="Email" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control has-feedback-left" id="password" name="password" placeholder="Password" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Confirm Password</label>
+                                    <input type="password" class="form-control has-feedback-left" id="conf_password" name="conf_password" placeholder="Re-Password" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Telepon Perusahaan</label>
+                                    <input type="text" class="form-control has-feedback-left" name="telp" placeholder="Telepon Perusahaan" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Alamat Perusahaan</label>
+                                    <textarea type="text" class="form-control has-feedback-left" placeholder="Alamat Perusahaan" name="alamat" required=""></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label>Nama PIC / CP / Penanggung Jawab Akun</label>
+                                    <input type="text" class="form-control has-feedback-left" name="nama_pj" placeholder="Nama Penanggung Jawab" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Telepon PIC / CP / Penanggung Jawab Akun</label>
+                                    <input type="text" class="form-control has-feedback-left" name="telp_pj" placeholder="Telepon Penanggung Jawab" required="" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Upload Logo Perusahaan</label>
+                                    <input type="file" class="dropify" height="100" name="logo" data-max-file-size="1M" data-allowed-file-extensions="jpg jpeg png" required="">
                                 </div>
                                 <div class="alert alert-warning" role="alert">
                                     <input type="checkbox" class="custom-checkbox" name="nama_syarat" required> Saya menyetujui bahwa telah melengkapi isian data pendaftaran ini dengan benar</input>
