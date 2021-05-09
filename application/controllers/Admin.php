78f190b38d8f0ce34310ac4767ca92bf3ec063f1
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-05-10 00:03:54 +0700

    kampus_merdeka

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -66,10 +66,11 @@ class Admin extends CI_Controller
                 $url = "https://tracerstudy.itenas.ac.id/cdc/alumni";
                 $get_url = file_get_contents($url);
                 $data['alumni'] = json_decode($get_url);
-                $this->load->view('admin/templates/header');
-                $this->load->view('admin/tracer', $data);
-                $this->load->view('admin/templates/js');
-                $this->load->view('admin/templates/footer');
+                echo $data['alumni'];
+                // $this->load->view('admin/templates/header');
+                // $this->load->view('admin/tracer', $data);
+                // $this->load->view('admin/templates/js');
+                // $this->load->view('admin/templates/footer');
             } else {
                 redirect('login');
             }
