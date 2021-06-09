fc1864aedc9116921cfe4a0d2e436abf9ed03b03
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-06-09 16:51:05 +0700

    test

--- application/views/admin/pendaftar.php
+++ application/views/admin/pendaftar.php
@@ -68,7 +68,7 @@
                                                 <?php foreach ($berkas as $r) {
                                                     foreach ($data as $d) {
                                                         if (($r->nama_syarat == $d->nama_berkas) && ($row->id_akun == $d->id_akun)) { ?>
-                                                            <td><a href="<?php echo base_url('assets/upload/berkas/' . $d->file)
+                                                            <td><a href="<?php echo base_url('halaman/download/' . $d->file)
                                                                             ?>">Ada</a></td>
                                                         <?php  }
                                                         ?>
