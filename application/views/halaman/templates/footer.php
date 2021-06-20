889da4e16c40586270246c04019ad659a23558fc
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-06-20 12:05:18 +0700

    test

--- application/views/halaman/templates/footer.php
+++ application/views/halaman/templates/footer.php
@@ -71,6 +71,7 @@
 
 <!-- Vendor JS Files -->
 <script src="<?php echo base_url() ?>assets/home/assets/vendor/jquery/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="<?php echo base_url() ?>assets/home/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url() ?>assets/home/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
 <script src="<?php echo base_url() ?>assets/home/assets/vendor/php-email-form/validate.js"></script>
